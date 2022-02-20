b7af777ba09d5494d83ce2db785a7a0267011549
Arslan Abdullaev <ars352@yahoo.com>
2022-02-21 01:08:10 +0300

    Initial Commit

---
+++ TrueTrafficLightAppSwiftUI/TrueTrafficLightAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueTrafficLightAppSwiftUIApp.swift
+//  TrueTrafficLightAppSwiftUI
+//
+//  Created by Arslan Abdullaev on 21.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrueTrafficLightAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
